047e628d61c018f7d19c9c6a4c666734d5f10b37
daoyu <daoyu.yao@etu.sorbonne-universite.fr>
2023-09-27 12:26:39 +0200

    1111

---
+++ partie_1/lab3/test.py
@@ -0,0 +1,10 @@
+
+# Trouver le produit de deux nombres entiers
+nbr1 = input('Entrez le premier nombre: ')
+nbr2 = input('Entrez le deuxième nombre: ')
+
+# Additionner les deux nombres 
+produit = int(nbr1) * int(nbr2)
+
+# Afficher la somme
+print('Le produit de {0} et  {1} est {2}'.format(nbr1, nbr2, produit))
