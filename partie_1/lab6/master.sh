1a6e977b33ca5659da719c1e04fd3ac79cde6120
daoyu <Daoyu.yao@etu.sorbonne-universite.fr>
2023-10-19 11:28:40 +0200

    q1

---
+++ partie_1/lab6/master.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# indiquer le type de shell 
+
+lab5_chemin="/home/daoyu/Desktop/MU5EE05/partie_1/lab5"
+# le chemin du répertoire du lab5
+
+ls $lab5_chemin
+# lister les fichiers de lab5
+
+mastersyscom="Hello Embedded Linux"
+#créer une variable appelée "mastersyscom" et stocker "Hello Embedded Linux"
